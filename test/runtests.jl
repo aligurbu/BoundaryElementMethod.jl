ab99cbf6e03074ba2464c242216f79e761bf5bf6
aligurbu <aligurbu@buffalo.edu>
2022-10-19 20:17:16 -0700

    Set up testing folder

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,9 @@
-using BoundaryElementMethod
-using Test
+using Test, SafeTestsets
 
 @testset "BoundaryElementMethod.jl" begin
     # Write your tests here.
+end
+
+@safetestset "This is only a test" begin
+    include("ThisIsOnlyATest.jl")
 end
